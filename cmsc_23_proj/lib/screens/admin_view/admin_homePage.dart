f0be3d62df0f8a03b58fd65c760274e0a49cba78
Jem De Venecia <jmdevenecia@up.edu.ph>
2024-05-15 19:48:33 +0800

    ui: created initial design of admin homepage

--- cmsc_23_proj/lib/screens/admin_view/admin_homePage.dart
+++ cmsc_23_proj/lib/screens/admin_view/admin_homePage.dart
@@ -11,7 +11,31 @@ class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Admin Home Page"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/ViewOrgsDonaPage');
+            }, 
+            child: const Text("View All Organizations and Donations")
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/OrgApprovalPage');
+            }, 
+            child: const Text("Organization Approval Page")
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/ViewOrgsDonaPage');
+            }, 
+            child: const Text("View All Donors")
+          )
+        ],
+      )
     );
   }
 }
