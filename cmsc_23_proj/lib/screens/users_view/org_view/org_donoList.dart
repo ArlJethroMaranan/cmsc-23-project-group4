1cd33cfbf1b492f20d0657b412231116b213aa8c
Meromeroks <amocampo9@up.edu.ph>
2024-05-17 14:23:03 +0800

    fixed radio to be dynamic across each donation

--- cmsc_23_proj/lib/screens/users_view/org_view/org_donoList.dart
+++ cmsc_23_proj/lib/screens/users_view/org_view/org_donoList.dart
@@ -12,13 +12,16 @@ List<String> donoStatus = ['Pending','Confirmed','Scheduled for pickup','Complet
 
 class _DonationListState extends State<DonationList> {
 
-  String currentStatus = donoStatus[0]; // set default of donostatus to pending
-
-  final List<List<dynamic>> donations = [ // list that holds the data to be inserted to expansion tile
-    ["Help Jonathan Joestar finish his house.", 2500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et aliquet lectus, id ornare ligula. Aenean sit amet tempor ipsum. Vestibulum maximus egestas leo sed fermentum. Mauris vel sapien placerat enim viverra euismod. Ut eu elementum mi. Integer euismod ipsum id pellentesque ornare. Nullam ac tellus interdum urna placerat bibendum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt justo nec eros convallis gravida. Sed at magna eu sem efficitur tempor in ac metus. Aliquam bibendum, neque dictum efficitur lacinia, nulla nisi euismod tellus, a malesuada sapien nisl non est. Nam placerat dolor sapien, ac tempor neque facilisis id."],
-    ["Dummy name 2", 2500, "description"],
-    ["Dummy name 3", 2500, "description"],
-    ["Dummy name 4", 2500, "description"],
+  final List<Map<String, dynamic>> donations = [ // list that holds the data to be inserted to expansion tile
+    {
+      "title": "Help Jonathan Joestar finish his house.",
+      "amount": 2500, 
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et aliquet lectus, id ornare ligula. Aenean sit amet tempor ipsum. Vestibulum maximus egestas leo sed fermentum. Mauris vel sapien placerat enim viverra euismod. Ut eu elementum mi. Integer euismod ipsum id pellentesque ornare. Nullam ac tellus interdum urna placerat bibendum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt justo nec eros convallis gravida. Sed at magna eu sem efficitur tempor in ac metus. Aliquam bibendum, neque dictum efficitur lacinia, nulla nisi euismod tellus, a malesuada sapien nisl non est. Nam placerat dolor sapien, ac tempor neque facilisis id.",
+      "status": donoStatus[0] // set default status to pending
+    },
+    {"title": "Dummy name 2", "amount": 2500, "description": "description", "status": donoStatus[0]},
+    {"title": "Dummy name 3", "amount": 2500, "description": "description", "status": donoStatus[0]},
+    {"title": "Dummy name 4", "amount": 2500, "description": "description", "status": donoStatus[0]},
   ];
 
   @override
@@ -36,11 +39,11 @@ class _DonationListState extends State<DonationList> {
         itemBuilder: (context, index) {
           final donation = donations[index];
           return Card( // Wrapped expansion tile with Card
-          color: Color.fromARGB(255, 214, 126, 62),
+            color: const Color.fromARGB(255, 214, 126, 62),
             child: ExpansionTile(
-              backgroundColor: Color.fromARGB(255, 219, 110, 32),
+              backgroundColor: const Color.fromARGB(255, 219, 110, 32),
               title: Text(
-                donation[0], // name
+                donation["title"], // name
                 style: const TextStyle(
                   color: Colors.white,
                   fontSize: 18,
@@ -48,7 +51,7 @@ class _DonationListState extends State<DonationList> {
                 ),
               ),
               subtitle: Text(
-                "Current Amount Raised: ${donation[1]}", // amount
+                "Current Amount Raised: ${donation["amount"]}", // amount
                 style: const TextStyle(
                   color: Colors.white,
                   fontSize: 16,
@@ -58,7 +61,7 @@ class _DonationListState extends State<DonationList> {
                 Padding(
                   padding: const EdgeInsets.all(16),
                   child: Text(
-                    donation[2], // description
+                    donation["description"], // description
                     style: const TextStyle(
                       color: Colors.white,
                       fontSize: 16,
@@ -74,7 +77,7 @@ class _DonationListState extends State<DonationList> {
                     fit: BoxFit.cover,
                   ),
                 ),
-                //Created Radios for the donation status
+                // Created Radios for the donation status
                 const Padding(
                   padding: EdgeInsets.all(16),
                   child: Text(
@@ -85,95 +88,25 @@ class _DonationListState extends State<DonationList> {
                     ),
                   ),
                 ),
-                ListTile(
-                  title: const Text(
-                    'Pending',
-                    style: TextStyle( // change style of text
-                      color: Colors.white,
-                      fontSize: 16,
+                Column(
+                  children: donoStatus.map((status) => ListTile( // convert each string status in donoStatus to list tile
+                    title: Text(
+                      status,
+                      style: const TextStyle( 
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                  leading: Radio(
-                    value: donoStatus[0], // value is set to it;s index
-                    groupValue: currentStatus, // determine which is currently selected
-                    onChanged: (value) {
-                      setState(() {
-                        currentStatus = value.toString(); // when clicked changed currentstatus
-                      });
-                    },
-                  )
-                ),
-                ListTile(
-                  title: const Text(
-                    'Confirmed',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                    leading: Radio<String>(
+                      value: status,
+                      groupValue: donation["status"], // points to currently picked
+                      onChanged: (value) {
+                        setState(() {
+                          donation["status"] = value!; 
+                        });
+                      },
                     ),
-                  ),
-                  leading: Radio(
-                    value: donoStatus[1],
-                    groupValue: currentStatus,
-                    onChanged: (value) {
-                      setState(() {
-                        currentStatus = value.toString();
-                      });
-                    },
-                  )
-                ),
-                ListTile(
-                  title: const Text(
-                    'Scheduled for pickup',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  leading: Radio(
-                    value: donoStatus[2],
-                    groupValue: currentStatus,
-                    onChanged: (value) {
-                      setState(() {
-                        currentStatus = value.toString();
-                      });
-                    },
-                  )
-                ),
-                ListTile(
-                  title: const Text(
-                    'Complete',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  leading: Radio(
-                    value: donoStatus[3],
-                    groupValue: currentStatus,
-                    onChanged: (value) {
-                      setState(() {
-                        currentStatus = value.toString();
-                      });
-                    },
-                  )
-                ),
-                ListTile(
-                  title: const Text(
-                    'Canceled',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  leading: Radio(
-                    value: donoStatus[4],
-                    groupValue: currentStatus,
-                    onChanged: (value) {
-                      setState(() {
-                        currentStatus = value.toString();
-                      });
-                    },
-                  )
+                  )).toList(), // change the iterable returned by .map to list again so that it can be accepted by column
                 ),
               ],
             ),
