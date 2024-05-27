ba5f10c2c83fbef759fc46eaa9f1f4c96f2d469a
Meromeroks <amocampo9@up.edu.ph>
2024-05-27 21:06:41 +0800

    Profile Organization: info comes from database, and there are radio buttons to set status of donation org. (currently accessing only one ORG for now as I set the firebase to a specific orgID since wala pa auth)

--- cmsc_23_proj/lib/screens/users_view/org_view/org_profile.dart
+++ cmsc_23_proj/lib/screens/users_view/org_view/org_profile.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cmsc_23_proj/screens/users_view/org_view/drawer.dart';
+import 'package:cmsc_23_proj/screens/users_view/org_view/orgModel/profileOrg.dart';
+import 'package:cmsc_23_proj/screens/users_view/org_view/orgProvider/providerProfileOrg.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfileOrg extends StatefulWidget {
   const ProfileOrg({super.key});
@@ -8,9 +12,14 @@ class ProfileOrg extends StatefulWidget {
   State<ProfileOrg> createState() => _ProfileOrgState();
 }
 
+List<String> statusForDono = ['Open','Close'];
+
 class _ProfileOrgState extends State<ProfileOrg> {
   @override
   Widget build(BuildContext context) {
+
+    Stream<QuerySnapshot> profileOrgStream = context.watch<OrgProfileProvider>().profileOrg;
+
     return Scaffold(
       drawer: const DrawerWidget(),
       appBar: AppBar(
@@ -19,7 +28,24 @@ class _ProfileOrgState extends State<ProfileOrg> {
         foregroundColor: Colors.white, // Text and icon color
       ),
       backgroundColor: Color.fromARGB(255, 229, 239, 95),
-      body: Center(
+      body: StreamBuilder(
+        stream: profileOrgStream,
+        builder: (context, snapshot) { // snapshot cases 
+          if (snapshot.hasError){
+            return Center(child: Text("Error encountered! ${snapshot.error}"));
+          } else if (snapshot.connectionState == ConnectionState.waiting){
+            return Center (child: CircularProgressIndicator(),);
+          } else if (!snapshot.hasData){
+            return Center(child: Text("No Donations Found"));
+          }
+
+      return ListView.builder(
+        itemCount: snapshot.data!.docs.length, //length based on donation list
+        itemBuilder: (context, index) {
+        
+        ProfileModelOrg temp = ProfileModelOrg.fromJson(snapshot.data!.docs[index].data() as Map<String, dynamic>);
+      
+      return Center(
           child: Column(
               children: [
                 const SizedBox(height: 30),
@@ -37,28 +63,28 @@ class _ProfileOrgState extends State<ProfileOrg> {
                 ),
                 const SizedBox(height: 30),
                 Container(
-                  height: 390,
+                  height: 430,
                   width: 370,
                   decoration: const BoxDecoration(
                   color: Color.fromARGB(255, 232, 130, 57),
                   // borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),
                   borderRadius: BorderRadius.all(Radius.circular(50)),
                   ),
-                  child: const Column (
+                  child: Column (
                     children: [
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
 
-                      Center(
+                      const Center(
                         child: Text("testemail@gmail.com", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                       ),
-                      Center(
+                      const Center(
                         child: Text("1827-364-9582"),
                       ),
-                      SizedBox(height: 50),
+                      const SizedBox(height: 50),
 
                       Column(
                         children: [
-                          Padding(
+                          const Padding(
                             padding: EdgeInsets.only(left:20),
                             child: Row(
                               children: [
@@ -70,13 +96,13 @@ class _ProfileOrgState extends State<ProfileOrg> {
                             padding: EdgeInsets.only(left:20),
                             child: Row(
                               children: [
-                                Text('name'),
+                                Text("${temp.name}"),
                               ],
                             ),  
                           ),
-                          SizedBox(height: 30),
+                          const SizedBox(height: 30),
 
-                          Padding(
+                          const Padding(
                             padding: EdgeInsets.only(left:20),
                             child: Row(
                               children: [
@@ -88,12 +114,12 @@ class _ProfileOrgState extends State<ProfileOrg> {
                             padding: EdgeInsets.only(left:20),
                             child: Row(
                               children: [
-                                Text('about'),
+                                Text('${temp.about}'),
                               ],
                             ),  
                           ),
-                          SizedBox(height: 30),
-                          Padding(
+                          const SizedBox(height: 30),
+                          const Padding(
                             padding: EdgeInsets.only(left:20),
                             child: Row(
                               children: [
@@ -101,13 +127,43 @@ class _ProfileOrgState extends State<ProfileOrg> {
                               ],
                             ),  
                           ),
-                          Padding(
-                            padding: EdgeInsets.only(left:20),
-                            child: Row(
-                              children: [
-                                Text('status'),
-                              ],
-                            ),  
+                          // Padding(
+                          //   padding: EdgeInsets.only(left:20),
+                          //   child: Row(
+                          //     children: [
+                          //       Text('status'),
+                          //     ],
+                          //   ),  
+                          // ),
+
+                          Column(
+                            children: statusForDono.map((status) { // convert each donostatus string to a list tile (used .map to access each individual strings in the donostatus list)
+                              return ListTile( // use list tile to add leading with radio
+                                title: Text(
+                                  status,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                leading: Radio<String>(
+                                  value: status,
+                                  groupValue: temp.status, // points to currently picked
+                                  onChanged: (value) {
+                                    setState(() {
+                                      temp.status = value!; // change the value 
+                                      // FirebaseFirestore.instance.collection('donations').doc(snapshot.data!.docs[index].id).update({'status': value}); // access the instance of the donations document using it's id then updating the status with the new value
+                                      
+                                      ProfileModelOrg data = ProfileModelOrg( // access status to a variable data from model
+                                        status: temp.status,
+                                      );
+                                      
+                                      context.read<OrgProfileProvider>().editStatus(snapshot.data!.docs[index].id, data); // call edit status to change the status
+                                    });
+                                  },
+                                ),
+                              );
+                            }).toList(), // convert toList since column can't accept iterable 
                           ),
                         ],
                       ),
@@ -117,6 +173,11 @@ class _ProfileOrgState extends State<ProfileOrg> {
                 ),
               ],
           ),
+        );
+        }
+      );
+     
+        }
       ),
     );
   }
